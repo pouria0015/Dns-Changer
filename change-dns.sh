40c76593e06e6a918e7a7145a450bb74b83da806
pouria0015 <pouria0015@gmail.com>
2024-02-19 22:46:14 +0330

    add: Adding the functions of selecting and receiving the menu and the possibility of exiting and repeatedly displaying the menu

--- change-dns.sh
+++ change-dns.sh
@@ -2,6 +2,30 @@
 
 source function.sh
 
+loop=0
+menuLogoView=12
+while [[ loop -eq 0 ]]; do
 
-menu
-dns_changer 
+if [[ $menuLogoView -eq 12 ]];then
+  menuLogo
+  menuLogoView=13
+elif [[ $menuLogoView -eq 13 ]]; then
+  echo =============================
+fi
+
+menuOptions
+
+num=$(getMenuNum)
+if [[ $num -eq 1 ]];then
+  viewMyDns
+elif [[ $num -eq 2 ]]; then
+  DnsDatabase
+elif [[ $num -eq 3 ]]; then
+  dns_changer
+elif [[ $num -eq 4 ]]; then
+  exit 0
+else
+  echo Enter the available options
+fi
+
+done
