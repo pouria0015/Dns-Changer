5e9b7f5d6ba745ddb97093e4407f40f878981907
pouria0015 <pouria0015@gmail.com>
2024-12-25 00:28:06 +0330

    Edit: solving code errors

--- dns-changer.sh
+++ dns-changer.sh
@@ -4,30 +4,31 @@ source function.sh
 
 loop=0
 menuLogoView=12
-while [[ loop -eq 0 ]]; do
+while [[ $loop -eq 0 ]]; do
 
-if [[ $menuLogoView -eq 12 ]];then
-  menuLogo
-  menuLogoView=13
-elif [[ $menuLogoView -eq 13 ]]; then
-  echo =============================
-fi
+  if [[ $menuLogoView -eq 12 ]]; then
+    menuLogo
+    menuLogoView=13
+  elif [[ $menuLogoView -eq 13 ]]; then
+    echo =============================
+  fi
 
-menuOptions
+  menuOptions
 
-num=$(getMenuNum)
-if [[ $num -eq 1 ]];then
-  viewMyDns
-elif [[ $num -eq 2 ]]; then
-  dns_changer
-elif [[ $num -eq 3 ]]; then
-  SelectDnsFromDatabase
-elif [[ $num -eq 4 ]]; then
-   resetDns
- elif [[ $num -eq 5 ]]; then
-   exit
- else
-  echo Enter the available options
-fi
+  num=$(getMenuNum)
+  if [[ $num -eq 1 ]]; then
+    viewMyDns
+  elif [[ $num -eq 2 ]]; then
+    dns_changer
+  elif [[ $num -eq 3 ]]; then
+    SelectDnsFromDatabase
+  elif [[ $num -eq 4 ]]; then
+    resetDns
+  elif [[ $num -eq 5 ]]; then
+    loop=1
+    exit
+  else
+    echo "Enter the available options"
+  fi
 
 done
